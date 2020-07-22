e590f8e2f487b39cc88e0b9e0f599425b39e1f53
Shivam Acharya <shivamacharya134@outlook.com>
2020-07-22 15:33:08 +0100

    yeet

--- backend/euro.py
+++ backend/euro.py
@@ -5,3 +5,32 @@ from sympy.stats import Normal, cdf
 from sympy import init_printing
 init_printing()
 
+def euro_vanilla_call(S, K, T, r, sigma):
+    
+    #S: spot price
+    #K: strike price
+    #T: time to maturity
+    #r: interest rate
+    #sigma: volatility of underlying asset
+    
+    d1 = (np.log(S / K) + (r + 0.5 * sigma ** 2) * T) / (sigma * np.sqrt(T))
+    d2 = (np.log(S / K) + (r - 0.5 * sigma ** 2) * T) / (sigma * np.sqrt(T))
+    
+    call = (S * si.norm.cdf(d1, 0.0, 1.0) - K * np.exp(-r * T) * si.norm.cdf(d2, 0.0, 1.0))
+    
+    return call
+
+def euro_vanilla_put(S, K, T, r, sigma):
+
+  #S: spot price
+  #K: strike price
+  #T: time to maturity
+  #r: interest rate
+  #sigma: volatility of underlying asset
+  
+  d1 = (np.log(S / K) + (r + 0.5 * sigma ** 2) * T) / (sigma * np.sqrt(T))
+  d2 = (np.log(S / K) + (r - 0.5 * sigma ** 2) * T) / (sigma * np.sqrt(T))
+  
+  put = (K * np.exp(-r * T) * si.norm.cdf(-d2, 0.0, 1.0) - S * si.norm.cdf(-d1, 0.0, 1.0))
+  
+  return put
